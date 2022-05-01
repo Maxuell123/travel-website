848e37d1c6c8a1b027a7c7f4969fe4f9e1aed1dc
Maxuell Lima <71743257+Maxuell123@users.noreply.github.com>
2022-05-01 11:50:23 -0300

    :up: atualização

--- js/script.js
+++ js/script.js
@@ -93,3 +93,51 @@ var swiper = new Swiper(".review-slider", {
         },
     },
 });
+
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    grabCursor: true,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,  
+        },
+        640: {
+            slidesPerView: 2,  
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+    },
+});
+
+
+var swiper = new Swiper(".blogs-slider", {
+    loop:true, 
+    grabCursor:true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+    },
+});
+
